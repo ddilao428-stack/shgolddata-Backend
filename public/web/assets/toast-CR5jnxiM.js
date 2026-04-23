1fba9f0898b8c56a5b3b8bc045a86167fe80417e
dev001 <dev001@cc.com>
2026-04-23 16:59:01 +0800

    update

--- public/web/assets/toast-CR5jnxiM.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index-Dm3ihOLB.js";function e(s){a({message:s,type:"warning",duration:2500})}export{e as s};
---
+++ public/web/assets/toast-D3Tm4D6K.js
@@ -0,0 +1,1 @@
+import{T as a}from"./index-Bcxkcg7O.js";function e(s){a({message:s,type:"warning",duration:2500})}export{e as s};
