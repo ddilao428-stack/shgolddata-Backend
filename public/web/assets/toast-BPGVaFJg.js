4f5b607ec3eca9e0c738ffe5f3aebe4c7e2a7d1c
dev001 <dev001@cc.com>
2026-04-23 15:46:49 +0800

    fix

--- public/web/assets/toast-BPGVaFJg.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index-ChT_PDeK.js";function e(s){a({message:s,type:"warning",duration:2500})}export{e as s};
---
+++ public/web/assets/toast-CR5jnxiM.js
@@ -0,0 +1,1 @@
+import{T as a}from"./index-Dm3ihOLB.js";function e(s){a({message:s,type:"warning",duration:2500})}export{e as s};
