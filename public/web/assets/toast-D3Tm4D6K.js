2170a263e67a6a8c5f674de49d5725790a2ef23c
dev001 <dev001@cc.com>
2026-04-28 00:03:41 +0800

    fix

--- public/web/assets/toast-D3Tm4D6K.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index-Bcxkcg7O.js";function e(s){a({message:s,type:"warning",duration:2500})}export{e as s};
---
+++ public/web/assets/toast-DJbRca82.js
@@ -0,0 +1,1 @@
+import{T as a}from"./index-DP-a05EO.js";function e(s){a({message:s,type:"warning",duration:2500})}export{e as s};
